4ae2e16f68ac4b4c204e8dea7d1ac7754c09db5f
Shujun Qi <shujun.qi@duke.edu>
2021-08-04 15:50:40 -0400

    webhook update

--- Kubernetes Settings/setup.sh
+++ Kubernetes Settings/setup.sh
@@ -2,7 +2,7 @@ sudo cp ca.crt /etc/kubernetes/pki/ca.crt
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
-sudo sh ../Webhook/deployment/webhook-create-signed-cert.sh
+sudo ../Webhook/deployment/webhook-create-signed-cert.sh
 sudo kubectl get secret admission-webhook-example-certs
 kubectl create -f ../Webhook/deployment/deployment.yaml
 kubectl create -f ../Webhook/deployment/service.yaml

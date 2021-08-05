9c4ce9e3424b5568d5ad5ba40d7e63696bd63472
Shujun Qi <shujun.qi@duke.edu>
2021-08-04 20:36:06 -0400

    update

--- Kubernetes Settings/clean.sh
+++ Kubernetes Settings/clean.sh
@@ -3,4 +3,5 @@ kubectl delete deployments admission-webhook-example-deployment
 kubectl delete secrets admission-webhook-example-certs
 kubectl delete validatingwebhookconfiguration validation-webhook-example-cfg
 kubectl delete service admission-webhook-example-svc
+kubectl label namespace default addmission-webhook-example-
 rm ../Webhook/deployment/validatingwebhook-ca-bundle.yaml

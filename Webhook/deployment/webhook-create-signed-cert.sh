cfe8df618a34a909c208b961ba4d8ff9377cc49c
Shujun Qi <shujun.qi@duke.edu>
2021-08-04 22:34:26 -0400

    update

--- Webhook/deployment/webhook-create-signed-cert.sh
+++ Webhook/deployment/webhook-create-signed-cert.sh
@@ -83,15 +83,15 @@ kubectl delete csr ${csrName} 2>/dev/null || true
 
 # create  server cert/key CSR and  send to k8s API
 cat <<EOF | kubectl create -f -
-apiVersion: certificates.k8s.io/v1
+apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
 metadata:
   name: ${csrName}
 spec:
   groups:
-  - system:authenticated
+  - system:nodes
   request: $(cat ${tmpdir}/server.csr | base64 | tr -d '\n')
-  signerName: kubernetes.io/legacy-unknown
+  
   usages:
   - digital signature
   - key encipherment

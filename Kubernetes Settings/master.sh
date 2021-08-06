b050370cda58f69131bf26c229b7f69dc5ea2b90
Shujun Qi <shujun.qi@duke.edu>
2021-08-05 21:34:49 -0400

    docker

--- Kubernetes Settings/master.sh
+++ Kubernetes Settings/master.sh
@@ -33,6 +33,34 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
+# cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+# overlay
+# br_netfilter
+# EOF
+
+# sudo modprobe overlay
+# sudo modprobe br_netfilter
+
+# # Setup required sysctl params, these persist across reboots.
+# cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+# net.bridge.bridge-nf-call-iptables  = 1
+# net.ipv4.ip_forward                 = 1
+# net.bridge.bridge-nf-call-ip6tables = 1
+# EOF
+
+# sudo mkdir -p /etc/containerd
+# containerd config default | sudo tee /etc/containerd/config.toml
+
+# sudo systemctl restart containerd
+
+dockercgs = '{"exec-opts": ["native.cgroupdriver=systemd"]}'
+sudo echo $dockercgs > /etc/docker/daemon.json
+sudo systemctl restart docker
+
+# Apply sysctl params without reboot
+# sudo sysctl --system
+
+
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
